88a8518ab1a1c1c832b0a77d22ecfbb16a219559
Mike Allen <mike@tecarta.com>
2017-01-27 15:45:35 -0800

    update events

--- lib/com/vaadin/polymer/elemental/MouseEvent.java
+++ lib/com/vaadin/polymer/elemental/MouseEvent.java
@@ -1,13 +1,14 @@
 package com.vaadin.polymer.elemental;
 
-import com.google.gwt.core.client.JsArray;
-import com.google.gwt.dom.client.Touch;
 import static jsinterop.annotations.JsPackage.GLOBAL;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 @JsType(isNative=true, namespace=GLOBAL)
-public interface TouchEvent extends Event {
+public interface MouseEvent extends Event {
     @JsProperty
-    JsArray<Touch> getTouches();
+    int getClientX();
+
+    @JsProperty
+    int getClientY();
 }
--- lib/com/vaadin/polymer/elemental/SelectionEvent.java
+++ lib/com/vaadin/polymer/elemental/SelectionEvent.java
@@ -1,13 +1,11 @@
 package com.vaadin.polymer.elemental;
 
-import com.google.gwt.core.client.JsArray;
-import com.google.gwt.dom.client.Touch;
 import static jsinterop.annotations.JsPackage.GLOBAL;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 @JsType(isNative=true, namespace=GLOBAL)
-public interface TouchEvent extends Event {
+public interface SelectionEvent extends Event {
     @JsProperty
-    JsArray<Touch> getTouches();
+    int getEventPhase();
 }
