fdec5b116b0307f76215262a062e09f7361ec62f
Mike Allen <mike@tecarta.com>
2017-03-29 14:36:25 -0700

    update Model

--- lib/com/vaadin/polymer/elemental/Model.java
+++ lib/com/vaadin/polymer/elemental/Model.java
@@ -7,11 +7,11 @@ public class Model extends JavaScriptObject {
    
    public final native Object getItem(String name) /*-{ return this[name]; }-*/;
    public final native Object getItem() /*-{ return this['item']; }-*/;
-   public final native Object getItemProperty(String property) /*-{ return this['item']['property']; }-*/;
-   public final native Object getItemProperty(String itemName, String property) /*-{ return this[itemName]['property']; }-*/;
-   public final native int getItemPropertyAsInt(String property) /*-{ return this['item']['property']; }-*/;
-   public final native int getItemPropertyAsInt(String itemName, String property) /*-{ return this[itemName]['property']; }-*/;
-   public final native String getItemPropertyAsString(String property) /*-{ return this['item']['property']; }-*/;
-   public final native String getItemPropertyAsString(String itemName, String property) /*-{ return this[itemName]['property']; }-*/;
+   public final native Object getItemProperty(String property) /*-{ return this['item'][property]; }-*/;
+   public final native Object getItemProperty(String itemName, String property) /*-{ return this[itemName][property]; }-*/;
+   public final native int getItemPropertyAsInt(String property) /*-{ return this['item'][property]; }-*/;
+   public final native int getItemPropertyAsInt(String itemName, String property) /*-{ return this[itemName][property]; }-*/;
+   public final native String getItemPropertyAsString(String property) /*-{ return this['item'][property]; }-*/;
+   public final native String getItemPropertyAsString(String itemName, String property) /*-{ return this[itemName][property]; }-*/;
    public final native int getIndex() /*-{ return this.index; }-*/;
 }
