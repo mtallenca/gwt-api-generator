1fa7aea8dd5c4fa5f383aac63f538b4a6b059aca
Mike Allen <mike@tecarta.com>
2017-04-13 11:29:13 -0700

    update get/set to be JavaScriptObject instead of Object

--- lib/com/vaadin/polymer/elemental/HTMLElement.java
+++ lib/com/vaadin/polymer/elemental/HTMLElement.java
@@ -25,10 +25,10 @@ public class HTMLElement extends Element {
     public final native void fire(String eventName, Object detail) /*-{ this.fire(eventName, detail); }-*/;
     public final native void fire(String eventName) /*-{ this.fire(eventName); }-*/;
 
-    public final native Object get(String propertyPath) /*-{ return this.get(propertyPath); }-*/;
+    public final native <T extends JavaScriptObject> T get(String propertyPath) /*-{ return this.get(propertyPath); }-*/;
     public final native String getAsString(String propertyPath) /*-{ return "" + this.get(propertyPath); }-*/;
     public final native int getAsInt(String propertyPath) /*-{ return this.get(propertyPath); }-*/;
-    public final native void set(String propertyPath, Object value) /*-{ this.set(propertyPath, value); }-*/;
+    public final native void set(String propertyPath, <T extends JavaScriptObject> T value) /*-{ this.set(propertyPath, value); }-*/;
     public final native void set(String propertyPath, int value) /*-{ this.set(propertyPath, value); }-*/;
 
     /**
--- lib/com/vaadin/polymer/elemental/Model.java
+++ lib/com/vaadin/polymer/elemental/Model.java
@@ -7,9 +7,9 @@ public class Model extends JavaScriptObject {
    
    public final native Model getParentModel() /*-{ return this.dataHost.dataHost; }-*/;
    public final native int getIndex() /*-{ return this.index; }-*/;
-   public final native Object get(String propertyPath) /*-{ return this.get(propertyPath); }-*/;
+   public final native <T extends JavaScriptObject> T get(String propertyPath) /*-{ return this.get(propertyPath); }-*/;
    public final native String getAsString(String propertyPath) /*-{ return "" + this.get(propertyPath); }-*/;
    public final native int getAsInt(String propertyPath) /*-{ return this.get(propertyPath); }-*/;
-   public final native void set(String propertyPath, Object value) /*-{ this.set(propertyPath, value); }-*/;
+   public final native void set(String propertyPath, <T extends JavaScriptObject> T value) /*-{ this.set(propertyPath, value); }-*/;
    public final native void set(String propertyPath, int value) /*-{ this.set(propertyPath, value); }-*/;
 }
