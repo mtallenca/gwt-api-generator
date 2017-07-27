251942d9ce85841d2d1446aec97c50f9a03c25aa
Mike Allen <mike@tecarta.com>
2017-07-27 15:50:17 -0700

    remove constants

--- lib/com/vaadin/polymer/elemental/WheelEvent.java
+++ lib/com/vaadin/polymer/elemental/WheelEvent.java
@@ -6,10 +6,6 @@ import jsinterop.annotations.JsType;
 
 @JsType(isNative=true, namespace=GLOBAL)
 public interface WheelEvent extends Event {
-    public static final int DOM_DELTA_PIXEL = 0;
-    public static final int DOM_DELTA_LINE = 1;
-    public static final int DOM_DELTA_PAGE = 2;
-
     @JsProperty
     int getDeltaX();
 
